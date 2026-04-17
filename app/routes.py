92ce91e5e8a8acefcb17c5022c0b6c88e8a4f7a7
indo <kabadyofficial@gmail.com>
2026-04-18 02:46:38 +0300

    Defined some major Class methods eg added add_event method to the Event class

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, flash, url_for
-from .models import User
+from flask_login import login_user, logout_user, login_required, current_user
+from .models import User, Event
 from app import db
 
 
 main = Blueprint('main', __name__) # same functionality/properties as the main 'app' instance
 
+
 @main.route("/")
 def home():
 	return render_template("home.html") # render some html
+
 
 @main.route("/signup", methods=["GET", "POST"])
 def signup():
@@ -18,6 +21,7 @@ def signup():
 		new_user.set_password(password)
 		db.session.add(new_user)
 		db.session.commit()
+		login_user(new_user) # log users in as soon as they sign up
 		flash("Account Created Successfully. Welcome onboard!")
 		return redirect(url_for("main.dashboard"))
 	return render_template("signup.html")
@@ -31,12 +35,13 @@ def login():
 		password = request.form['password']
 		user = User.query.filter_by(username=username).first()
 		if user and user.check_password(password):
+			login_user(user)
 			flash("Logged in Successfully. Welcome back!")
 			return redirect(url_for("main.dashboard"))
 
 		else:
 			flash("Invalid credentils. GET LOST!")
-			return redirect(url_for("main.home"))
+			return redirect(url_for("main.login"))
 
 	return render_template("login.html")
 
@@ -47,13 +52,34 @@ def dashboard():
 	return render_template("dashboard.html")
 # TODO: needs improvement
 
+@main.route("/event/new", methods=["GET", "POST"])
+def create_event():
+    if request.method == "POST":
+        new_event = Event(
+            title=request.form['title'],
+            description=request.form['description'],
+            location=request.form['location'],
+            capacity=int(request.form['capacity']),
+            category=request.form['category'],
+            organiser_id=1 # TODO: Change this to current_user.id after setting up Flask-Login
+        )
+        
+        db.session.add(new_event)
+        db.session.commit()
+        
+        flash("Event created successfully!")
+        return redirect(url_for("main.dashboard"))
+    
+    return render_template("create_event.html")
 
 @main.route("/logout", methods=["GET", "POST"])
 def logout():
-	#flash("Logged out Successfully. Come back again!!")
-	return render_template("logout.html")
+	logout_user() # end session
+	flash("Logged out Successfully. Come back again!!")
+	return redirect(url_for('main.home'))
 
-@main.route("/signout", methods=["GET", "POST"])
+
+@main.route("/signout", methods=["GET", "POST"]) # completely delete account
 def signout():
 	#flash("Signing out.. We are sad to see you leave!!")
-	return render_template("signout.html") # TODO: safely delete entries from the database
+	return redirect(url_for('main.home')) # TODO: safely delete entries from the database
