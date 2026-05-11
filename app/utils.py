8e428cc656961bc7aeb8221b20a37631795cced0
fadhili2430 <fadhilijonan19@gmail.com>
2026-05-11 20:05:03 +0300

    Add real Kenyan university GPS coords + 20 universities

--- app/utils.py
+++ app/utils.py
@@ -13,27 +13,48 @@ def populate_initial_data():
         {'name': 'Basketball', 'description': 'Basketball games and practice'},
         {'name': 'Study Group', 'description': 'Group study sessions and academic support'},
     ]
-    
+
     for act in activities:
         if not Activity.query.filter_by(name=act['name']).first():
-            activity = Activity(name=act['name'], description=act['description'])
-            db.session.add(activity) # it is a good idea to add the update to the session first and later commit
-    
-    # Populate Campus Areas
+            db.session.add(Activity(name=act['name'], description=act['description']))
+
+    # Campus Areas — MMU buildings + all major Kenyan university campuses
     areas = [
-        {'name': 'Main Block', 'description': 'Main academic building'},
-        {'name': 'Sports Centre', 'description': 'Sports facilities and gymnasium'},
-        {'name': 'Library Grounds', 'description': 'Library and surrounding areas'},
-        {'name': 'Cafeteria', 'description': 'Dining and social area'},
-        {'name': 'Lecture Room 1', 'description': 'Lecture hall for classes and events'},
-        {'name': 'Computer Lab', 'description': 'Computing and IT facilities'},
-        {'name': 'Student Union', 'description': 'Student activities and events'},
-        {'name': 'Dormitory Area', 'description': 'Student housing and residential area'},
+        # --- Multimedia University of Kenya (MMU) — Ongata Rongai, Nairobi ---
+        {'name': 'MMU - Main Block',       'description': 'Multimedia University main academic building'},
+        {'name': 'MMU - Sports Centre',    'description': 'MMU sports facilities and gymnasium'},
+        {'name': 'MMU - Library',          'description': 'MMU library and study grounds'},
+        {'name': 'MMU - Cafeteria',        'description': 'MMU dining and social area'},
+        {'name': 'MMU - Computer Lab',     'description': 'MMU computing and IT facilities'},
+        {'name': 'MMU - Student Centre',   'description': 'MMU student activities hub'},
+        {'name': 'MMU - Lecture Block',    'description': 'MMU lecture halls and classrooms'},
+        {'name': 'MMU - Hostels',          'description': 'MMU student residential area'},
+
+        # --- Other Kenyan Universities ---
+        {'name': 'University of Nairobi',              'description': 'Main campus, Nairobi CBD'},
+        {'name': 'Kenyatta University',                'description': 'Main campus, Kahawa, Nairobi'},
+        {'name': 'Strathmore University',              'description': 'Madaraka Estate, Nairobi'},
+        {'name': 'JKUAT',                              'description': 'Jomo Kenyatta University, Juja'},
+        {'name': 'Technical University of Kenya',      'description': 'Haile Selassie Avenue, Nairobi'},
+        {'name': 'USIU-Africa',                        'description': 'United States International University, Nairobi'},
+        {'name': 'Daystar University',                 'description': 'Athi River Campus, Machakos'},
+        {'name': 'Catholic University (CUEA)',         'description': 'Lang\'ata, Nairobi'},
+        {'name': 'Moi University',                     'description': 'Main campus, Eldoret'},
+        {'name': 'Maseno University',                  'description': 'Maseno, Kisumu County'},
+        {'name': 'Egerton University',                 'description': 'Njoro, Nakuru County'},
+        {'name': 'Dedan Kimathi University',           'description': 'Nyeri, Central Kenya'},
+        {'name': 'Mount Kenya University',             'description': 'Main campus, Thika'},
+        {'name': 'Kisii University',                   'description': 'Kisii Town, Kisii County'},
+        {'name': 'Laikipia University',                'description': 'Nyahururu, Laikipia County'},
+        {'name': 'Machakos University',                'description': 'Machakos Town'},
+        {'name': 'Chuka University',                   'description': 'Chuka, Tharaka Nithi County'},
+        {'name': 'Kirinyaga University',               'description': 'Kutus, Kirinyaga County'},
+        {'name': 'Pwani University',                   'description': 'Kilifi, Coast Region'},
+        {'name': 'Jaramogi Oginga Odinga University', 'description': 'Bondo, Siaya County'},
     ]
-    
+
     for area in areas:
         if not CampusArea.query.filter_by(name=area['name']).first():
-            campus_area = CampusArea(name=area['name'], description=area['description'])
-            db.session.add(campus_area)
-    
+            db.session.add(CampusArea(name=area['name'], description=area['description']))
+
     db.session.commit()
