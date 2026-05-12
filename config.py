2df7dc3bf6a143cd45e691c479eb2534f9b5d87c
fadhili2430 <fadhilijonan19@gmail.com>
2026-05-12 09:59:19 +0300

    Clean up AI/Replit annotations: config.py

--- config.py
+++ config.py
@@ -1,20 +1,19 @@
 import os
 
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'tung-tung-tung-sahur'  # TODO: remove this
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # Performance & connection pooling
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        'pool_size': 10,
-        'pool_recycle': 3600,
-        'pool_pre_ping': True,
-        'connect_args': {'timeout': 15}
-    }
-    
-    # Session config
-    PERMANENT_SESSION_LIFETIME = 3600  # 1 hour
-    SESSION_COOKIE_SECURE = False  # Set to True if using HTTPS
-    SESSION_COOKIE_HTTPONLY = True
-    SESSION_COOKIE_SAMESITE = 'Lax'
+  class Config:
+      SECRET_KEY = os.environ.get('SECRET_KEY') or 'tung-tung-tung-sahur'
+      SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///site.db'
+      SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+      SQLALCHEMY_ENGINE_OPTIONS = {
+          'pool_size': 10,
+          'pool_recycle': 3600,
+          'pool_pre_ping': True,
+          'connect_args': {'timeout': 15}
+      }
+
+      PERMANENT_SESSION_LIFETIME = 3600
+      SESSION_COOKIE_SECURE = False
+      SESSION_COOKIE_HTTPONLY = True
+      SESSION_COOKIE_SAMESITE = 'Lax'
+  
