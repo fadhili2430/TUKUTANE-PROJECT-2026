44bcdd663ce6c6a24a4b708b727a62f134ba895f
fadhili2430 <fadhilijonan19@gmail.com>
2026-05-13 07:15:28 +0300

    feat: add ForgotPasswordForm and ResetPasswordForm

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, DateField, TimeField, IntegerField, SelectMultipleField
-from wtforms.validators import DataRequired, Email, Length, NumberRange
+from wtforms.validators import DataRequired, Email, Length, NumberRange, EqualTo
 from .models import Activity, CampusArea
 
 class SignupForm(FlaskForm):
@@ -20,6 +20,15 @@ class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class ForgotPasswordForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    submit = SubmitField('Send Reset Link')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('New Password', validators=[DataRequired(), Length(min=6)])
+    confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match')])
+    submit = SubmitField('Reset Password')
 
 class EventForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired(), Length(max=100)])
