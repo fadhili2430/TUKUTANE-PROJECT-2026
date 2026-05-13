6a6ce9d0209eb3ee01a5714a129c9c95def4a0c1
fadhili2430 <fadhilijonan19@gmail.com>
2026-05-13 07:15:28 +0300

    feat: add Flask-Mail to app factory

--- app/__init__.py
+++ app/__init__.py
@@ -2,30 +2,29 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import os
 
-db = SQLAlchemy() # if we later scale up, replace simple sqlalchemy with mysql 'I guess'
+db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
+mail = Mail()
 
 def start_app():
     basedir = os.path.abspath(os.path.dirname(__file__))
-    app = Flask(__name__, 
-                template_folder=os.path.join(basedir, '../templates'), 
-                static_folder=os.path.join(basedir, '../static')) # handle a variety of user machines
-    
+    app = Flask(__name__,
+                template_folder=os.path.join(basedir, '../templates'),
+                static_folder=os.path.join(basedir, '../static'))
+
     app.config.from_object('config.Config')
 
-    # DB initialization
     db.init_app(app)
-    migrate.init_app(app, db)  
-
-    # Login Manager
+    migrate.init_app(app, db)
     login_manager.init_app(app)
-    login_manager.login_view = 'main.login' # send users here if not logged in
+    login_manager.login_view = 'main.login'
+    mail.init_app(app)
 
-    # Register routes
     from app.routes import main
     app.register_blueprint(main)
-    
+
     return app
